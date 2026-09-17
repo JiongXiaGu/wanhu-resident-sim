7260e366c6778e9c13dcd0dff0c525f056a6f6f7
JiongXiaXia <jiongxiagu@outlook.com>
2026-09-17 14:26:50 +0800

    Load resident life log styles

--- Web/src/main.tsx
+++ Web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './game-validation.css';
+import './life-log.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
